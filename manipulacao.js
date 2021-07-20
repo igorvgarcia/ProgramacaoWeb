5ef8b669d87acaeec35b89255e280641ca7c9a12
igorvgarcia <igorvgarcia@outlook.com>
2021-07-20 20:12:43 -0300

    Atualização da página de Filmes

---
+++ manipulacao.js
@@ -0,0 +1,107 @@
+window.onload = function() {
+    var filme1 = document.getElementById("filme1Check")
+
+    filme1.addEventListener("click", clickFilme1)
+
+    var filme1 = document.getElementById("filme1Check")
+
+    filme1.addEventListener("click", filme1Assistido)
+
+    var filme2 = document.getElementById("filme2Check")
+
+    filme2.addEventListener("click", clickFilme2)
+
+    var filme2 = document.getElementById("filme2Check")
+
+    filme2.addEventListener("click", filme2Assistido)
+
+    var filme3 = document.getElementById("filme3Check")
+
+    filme3.addEventListener("click", clickFilme3)
+
+    var filme3 = document.getElementById("filme3Check")
+
+    filme3.addEventListener("click", filme3Assistido)
+
+    var filme4 = document.getElementById("filme4Check")
+
+    filme4.addEventListener("click", clickFilme4)
+
+    var filme4 = document.getElementById("filme4Check")
+
+    filme4.addEventListener("click", filme4Assistido)
+
+    var filme5 = document.getElementById("filme5Check")
+
+    filme5.addEventListener("click", clickFilme5)
+
+    var filme5 = document.getElementById("filme5Check")
+
+    filme5.addEventListener("click", filme5Assistido)
+
+    var filme6 = document.getElementById("filme6Check")
+
+    filme6.addEventListener("click", clickFilme6)
+
+    var filme6 = document.getElementById("filme6Check")
+
+    filme6.addEventListener("click", filme6Assistido)
+}
+
+
+
+function clickFilme1() {
+    alert('Filme 1 assistido')
+
+}
+
+function filme1Assistido() {
+    document.querySelector("#filme1Check").innerHTML = 'Filme assistido'
+}
+
+
+
+function clickFilme2() {
+    alert('Filme 2 assistido')
+
+}
+
+function filme2Assistido() {
+    document.querySelector("#filme2Check").innerHTML = 'Filme assistido'
+}
+
+function clickFilme3() {
+    alert('Filme 3 assistido')
+
+}
+
+function filme3Assistido() {
+    document.querySelector("#filme3Check").innerHTML = 'Filme assistido'
+}
+
+function clickFilme4() {
+    alert('Filme 4 assistido')
+
+}
+
+function filme4Assistido() {
+    document.querySelector("#filme4Check").innerHTML = 'Filme assistido'
+}
+
+function clickFilme5() {
+    alert('Filme 5 assistido')
+
+}
+
+function filme5Assistido() {
+    document.querySelector("#filme5Check").innerHTML = 'Filme assistido'
+}
+
+function clickFilme6() {
+    alert('Filme 6 assistido')
+
+}
+
+function filme6Assistido() {
+    document.querySelector("#filme6Check").innerHTML = 'Filme assistido'
+}
